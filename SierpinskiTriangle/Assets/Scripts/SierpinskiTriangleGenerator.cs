1b85ba71fb13870b75fa5432316e30a99baac061
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2022-09-20 12:25:45 +0100

    Visualise Vertices of first Division
    Creates the vertices for the first new generation of the triangle, and visualises them with Gizmos. I know the generation of this triangle can be done in different ways, but I wanted to do it the way we visually interpret the triangle, as its easier to explain in a video

--- SierpinskiTriangle/Assets/Scripts/SierpinskiTriangleGenerator.cs
+++ SierpinskiTriangle/Assets/Scripts/SierpinskiTriangleGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class SierpinskiTriangleGenerator : MonoBehaviour
 {
     [SerializeField] private float _radius;
 
+    private Mesh _mesh;
     private MeshFilter _meshFilter;
     private MeshRenderer _meshRenderer;
 
@@ -19,7 +21,7 @@ public class SierpinskiTriangleGenerator : MonoBehaviour
     private void Start()
     {
         // Create the Mesh for the triangles
-        Mesh mesh = new Mesh();
+        _mesh = new Mesh();
 
         // Create Vertices, UVs, and Triangles array
         Vector3[] vertices = new Vector3[3];
@@ -52,9 +54,71 @@ public class SierpinskiTriangleGenerator : MonoBehaviour
         triangles[2] = 2;
 
         // Initialise the Mesh with a single triangle
-        mesh.vertices = vertices;
-        mesh.uv = uv;
-        mesh.triangles = triangles;
-        _meshFilter.mesh = mesh;
+        _mesh.vertices = vertices;
+        _mesh.uv = uv;
+        _mesh.triangles = triangles;
+        _meshFilter.mesh = _mesh;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextGeneration();
+        }
+    }
+
+    private void NextGeneration()
+    {
+        Vector3[] oldVertices = _mesh.vertices;
+        Vector3[] newVertices = new Vector3[oldVertices.Length * 2];
+        Array.Copy(oldVertices, newVertices, oldVertices.Length);
+
+        int[] oldTriangles = _mesh.triangles;
+        int[] newTriangles = new int[oldTriangles.Length * 3];
+
+        // For every triangle
+        for (int i = 0; i < oldTriangles.Length; i += 3)
+        {
+            // First, create the new vertices
+            // Get the indicies of the vertices of the triangle
+            int t1 = oldTriangles[i];
+            int t2 = oldTriangles[i + 1];
+            int t3 = oldTriangles[i + 2];
+
+            // Get the vertices of the triangle
+            Vector3 v1 = oldVertices[t1];
+            Vector3 v2 = oldVertices[t2];
+            Vector3 v3 = oldVertices[t3];
+
+            // Find the midpoints (new vertices)
+            Vector3 v4 = (v1 + v2) / 2;
+            Vector3 v5 = (v2 + v3) / 2;
+            Vector3 v6 = (v3 + v1) / 2;
+
+            // Get the indices of the new vertices
+            int t4 = oldVertices.Length + i / 3;
+            int t5 = t4 + 1;
+            int t6 = t5 + 1;
+
+            // Add the new vertices to the mesh
+            newVertices[t4] = v4;
+            newVertices[t5] = v5;
+            newVertices[t6] = v6;
+
+            _mesh.vertices = newVertices;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_mesh != null)
+        {
+            foreach (Vector3 v in _mesh.vertices)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(v, 0.2f);
+            }
+        }
     }
 }
